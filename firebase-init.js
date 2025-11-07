087a7a2fe343b30e0c6563cff986c8e14a9548db
maryaeduarda33 <maryaeduardawl88@gmail.com>
2025-11-07 17:39:02 +0000

    aa

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDHI8sWXCb7NIHGeuvVTdY_NkEBOk9s-ns",
-    authDomain: "projetogamificaeduk.firebaseapp.com",
-    projectId: "projetogamificaeduk",
-    storageBucket: "projetogamificaeduk.appspot.com",
-    messagingSenderId: "402375253085",
-    appId: "1:402375253085:web:c882c3e23942e5874801b4",
-    measurementId: "G-2TY4YPL585"
+    apiKey: "AIzaSyBzZGF9uwkfa-jy8ELYpMb8yzVbv6B55Ps",
+    authDomain: "projeto-login-c536f.firebaseapp.com",
+    projectId: "projeto-login-c536f",
+    storageBucket: "projeto-login-c536f.firebasestorage.app",
+    messagingSenderId: "968048609985",
+    appId: "1:968048609985:web:e95e400673a1cadfbce240",
+    measurementId: "G-SKWNX4H8QX"
+
   };
 
 firebase.initializeApp(firebaseConfig); 
